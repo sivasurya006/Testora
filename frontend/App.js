adcdf1c44e15821f06ec4b80475020ca17e6930d
sivasurya006 <titansurya88@gmail.com>
2026-01-23 04:18:20 +0530

    frontend app created

---
+++ frontend/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <Text>Open up App.js to start working on your app!</Text>
+  );
+}
+
