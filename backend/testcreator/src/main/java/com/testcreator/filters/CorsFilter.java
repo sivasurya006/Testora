228d5f3d575b075bc0998cc438d1afbd9992cbaf
sivasurya006 <titansurya88@gmail.com>
2026-02-26 20:38:05 +0530

    Cors filter old code updated

--- backend/testcreator/src/main/java/com/testcreator/filters/CorsFilter.java
+++ backend/testcreator/src/main/java/com/testcreator/filters/CorsFilter.java
@@ -1,53 +1,41 @@
 package com.testcreator.filters;
 
 import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class CorsFilter extends HttpFilter implements Filter {
-       
-    public CorsFilter() {
-        super();
-    }
+
 	public void destroy() {
-		
 	}
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-	        throws IOException, ServletException {
+			throws IOException, ServletException {
 
-	    HttpServletRequest request = (HttpServletRequest) req;
-	    HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
 
-	    String origin = request.getHeader("Origin");
-	    if ("https://testora-kqvp.onrender.com".equals(origin)) {
-	        response.setHeader("Access-Control-Allow-Origin", origin);
-	        response.setHeader("Access-Control-Allow-Credentials", "true");
-	    }
+		String origin = request.getHeader("Origin");
+		if ("https://testora-kqvp.onrender.com".equals(origin)) {
+			response.setHeader("Access-Control-Allow-Origin", origin);
+			response.setHeader("Access-Control-Allow-Credentials", "true");
+		}
 
-	    response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE, OPTIONS");
-	    response.setHeader("Access-Control-Allow-Headers","Content-Type, Authorization, X-Client-Type, X-ClassroomId, X-TestId, X-QuestionId, X-AttemptId");
+		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Content-Type, Authorization, X-Client-Type, X-ClassroomId, X-TestId, X-QuestionId, X-AttemptId");
 
-	    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-	        response.setStatus(HttpServletResponse.SC_OK);
-	        return; 
-	    }
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+			return;
+		}
 
-	    chain.doFilter(req, res);
+		chain.doFilter(req, res);
 	}
-
 
 	public void init(FilterConfig fConfig) throws ServletException {
-		System.out.println("inited");
 	}
-
 }
